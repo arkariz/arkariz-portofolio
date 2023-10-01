c98103ccf52ad75b52aeffc6d791ce67314a16e4
arkariz <muhammadrisky1401@gmail.com>
2023-10-01 17:38:41 +0700

    feat: add config theme, color, responsive

---
+++ lib/core/colors/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+LinearGradient get pinkpurple => const LinearGradient(
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+  colors: [Color(0XFFaa367c), Color(0XFF4a2fbd)],
+);
---
+++ lib/core/widgets/custom_text.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const h1 = 32.0;
+const h2 = 24.0;
+const h3 = 18.0;
+const h4 = 16.0;
+const h5 = 14.0;
+const h6 = 12.0;
+
+class CustomText extends StatelessWidget {
+  const CustomText({
+    super.key,
+    required this.text,
+    this.fontSize = h6,
+  });
+
+  final String text;
+  final double fontSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize,
+      ),
+    );
+  }
+}
