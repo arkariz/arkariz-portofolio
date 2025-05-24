d89b360744a10b4545f562e34b01c154dd2c270d
arkariz <muhammadrisky1401@gmail.com>
2025-05-24 09:33:32 +0700

    feat: update profile picture animation

--- lib/app/sections/home/home_screen_controller.dart
+++ lib/app/sections/home/home_screen_controller.dart
@@ -1,5 +1,23 @@
 import 'package:get/get.dart';
 
 class HomeScreenController extends GetxController {
-  final isHoverProfilePicture = false.obs;
+  RxBool isHoverProfilePicture = false.obs;
+  RxDouble profilePictureOffset = 0.0.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _startProfileAnimation();
+  }
+
+  void _startProfileAnimation() {
+    // Animate profile picture up and down
+    Future.doWhile(() async {
+      await Future.delayed(const Duration(milliseconds: 1500));
+      profilePictureOffset.value = 15;
+      await Future.delayed(const Duration(milliseconds: 1500));
+      profilePictureOffset.value = 0;
+      return true; // Continue the animation loop
+    });
+  }
 }
